30bda9ddc19a3b932f9ba705bb52b1df4c1a565a
nana72848e <d1041211005@student.untan.ac.id>
2023-08-29 14:54:39 +0700

    Menambahkan fungsi protein, ph, dan blood predict

--- src/predict.py
+++ src/predict.py
@@ -29,3 +29,50 @@ def predict_urobilinogen(urobilinogrn_result):
         return "Konsentrasi urobilinogen yang lebih tinggi dalam urine. Bisa menunjukkan masalah yang lebih serius dalam metabolisme bilirubin atau masalah hati."
     elif urobilinogrn_result == "12 (Pos)":
         return "Konsentrasi urobilinogen yang tinggi dalam urine. Bisa menjadi tanda masalah serius dalam metabolisme bilirubin atau gangguan hati yang perlu penanganan medis lebih lanjut."
+
+def predict_protein(protein_result):
+    if protein_result == "NEGATIF":
+        return " Biasanya, urin sehat tidak mengandung jumlah protein yang tinggi."
+    elif protein_result == "+/-":
+        return "Ini bisa mengindikasikan adanya protein, masih dapat dianggap sebagai tanda kecil proteinuria."
+    elif protein_result == "+":
+        return "Ini bisa mengindikasikan adanya proteinuria ringan, yang dapat terjadi dalam beberapa situasi, termasuk setelah aktivitas fisik intens atau demam."
+    elif protein_result == "++":
+        return "Ini menunjukkan adanya konsentrasi protein yang lebih tinggi dalam urin. Ini mungkin mengindikasikan adanya masalah kesehatan, seperti infeksi saluran kemih, tekanan darah tinggi, atau gangguan ginjal awal."
+    elif protein_result == "+++":
+        return "Ini menunjukkan tingkat protein yang lebih tinggi lagi dalam urin. Ini dapat mengindikasikan kondisi kesehatan yang lebih serius, seperti kerusakan ginjal yang lebih lanjut atau penyakit ginjal yang berpotensi lebih kompleks."
+    elif protein_result == "++++":
+        return "Ini indikasi serius dari masalah ginjal atau masalah kesehatan lainnya yang memengaruhi fungsi ginjal."
+def predict_ph(ph_result):
+    if ph_result == "5,0":
+        return "pH sangat asam, Ini mengindikasikan potensi masalah medis atau masalah diet tertentu."
+    elif ph_result == "6,0":
+        return "pH urin cenderung sedikit asam, tetapi masih berada dalam kisaran normal. Ini adalah kondisi umum dan biasanya tidak menimbulkan kekhawatiran besar."
+    elif ph_result == "6,5":
+        return "pH urin cenderung sedikit asam, tetapi masih berada dalam kisaran normal. Ini adalah kondisi umum dan biasanya tidak menimbulkan kekhawatiran besar."
+    elif ph_result == "7,0":
+        return "pH urin netral. Ini adalah kondisi yang dianggap normal. Sebagian besar orang memiliki pH urin sekitar 6.0 hingga 7.5."
+    elif ph_result == "7,5":
+        return "pH urin cenderung sedikit basa (alkalis). Ini masih berada dalam kisaran normal dan umumnya tidak menjadi masalah."
+    elif ph_result == "8,0":
+        return "pH urin cenderung sedikit basa (alkalis). Ini masih berada dalam kisaran normal dan umumnya tidak menjadi masalah."
+    elif ph_result == "9,0":
+        return "pH urin sangat basa. Seperti pH urin yang sangat asam, kondisi ini juga jarang terjadi dalam keadaan normal dan dapat mengindikasikan potensi masalah medis atau faktor diet yang ekstrem."
+
+def predict_blood(blood_result):
+    if blood_result == "NEGATIF":
+        return "NEGATIF berarti tidak ada deteksi darah yang signifikan dalam urin. Ini mengindikasikan bahwa urin tidak mengandung darah dalam jumlah yang dapat dideteksi oleh tes ini."
+    elif blood_result == "+/-":
+        return "Ini menunjukkan adanya sedikit jumlah darah dalam urin. Ini bisa mengindikasikan adanya jejak darah, tetapi jumlahnya sangat kecil sehingga tidak terlihat secara visual."
+    elif blood_result == "+":
+        return "Ini menunjukkan adanya darah dalam urin dalam jumlah sedikit. Ini bisa mengindikasikan adanya masalah seperti infeksi saluran kemih, batu ginjal, atau masalah lainnya yang mempengaruhi saluran kemih atau sistem ginjal."
+    elif blood_result == "++":
+        return "Ini menunjukkan adanya jumlah darah yang lebih besar dalam urin. Ini bisa mengindikasikan masalah yang lebih serius, seperti peradangan ginjal, infeksi yang lebih parah, atau gangguan ginjal lainnya."
+    elif blood_result == "+++":
+        return "Ini menunjukkan konsentrasi darah yang lebih tinggi lagi dalam urin. Ini bisa mengindikasikan kondisi yang lebih serius, seperti kerusakan ginjal yang signifikan atau masalah lain yang mempengaruhi sistem kemih."
+    elif blood_result == "5-10":
+        return "Ini menunjukkan jumlah sel darah merah yang cukup banyak"
+    elif blood_result == "50":
+        return ""
+    
+
