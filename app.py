b7ad31f1556e4884d315b5a7d625f12e3422241d
tafifa <tafifa09@gmail.com>
2023-09-02 00:54:22 +0700

    remove dipstick_analysis function

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-import cv2
-import numpy as np
-import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
-
-from src.pages.predict import rectangle, process_dipstick, predict_leukocyte, predict_nitrit, predict_urobilinogen, predict_protein, predict_ph, predict_blood, predict_specificgravity, predict_ketone, predict_bilirubin, predict_glukosa
+from src.pages.analysis import dipstick_analysis
 from src.pages.chatbot import chatbot
 
 hide_st_style = """ 
@@ -14,61 +10,6 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True) # hide streamlit menu
-    
-def dipstick_analysis():
-    st.markdown("<h1 style='text-align: center; margin-bottom: 1em;'>Dipstick Analysis</h1>", unsafe_allow_html=True)
-    st.write("Unggah gambar dan dapatkan hasil")
-
-    uploaded_image = st.file_uploader("Pilih gambar...", type=["jpg", "jpeg", "png"])
-
-    if uploaded_image is not None:
-        image = cv2.imdecode(np.frombuffer(uploaded_image.read(), np.uint8), 1)
-
-        st.write("Analysis Results:")
-
-        col1, col2 = st.columns([1, 10])
-
-        
-        with col1:
-            # Process the image and draw rectangles
-            image = rectangle(image)
-        
-            # Convert BGR image to RGB format
-            rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            
-            # Display the RGB image
-            st.image(rgb_image)
-            
-
-        with col2:
-            result = process_dipstick(image)
-
-            # Create a DataFrame for the results
-            result_df = pd.DataFrame(result.items(), columns=["Parameter", "Value"])
-            result_df['Value'] = result_df['Value'].apply(lambda x: x[0][0])  # Extracting the value from the 2D array
-            
-            result_df['Keterangan'] = result_df.apply(lambda row: predict_leukocyte(row['Value']) if row['Parameter'] == 'LEUKOSIT'
-                                          else predict_nitrit(row['Value']) if row['Parameter'] == 'NITRIT'
-                                          else predict_urobilinogen(row['Value']) if row['Parameter'] == 'UROBILINOGEN'
-                                          else predict_protein(row['Value']) if row['Parameter'] == 'PROTEIN'
-                                          else predict_ph(row['Value']) if row['Parameter'] == 'pH'
-                                          else predict_blood(row['Value']) if row['Parameter'] == 'BLOOD'
-                                          else predict_specificgravity(row['Value']) if row['Parameter'] == 'SG'
-                                          else predict_ketone(row['Value']) if row['Parameter'] == 'KETON'
-                                          else predict_bilirubin(row['Value']) if row['Parameter'] == 'BILIRUBIN'
-                                          else predict_glukosa(row['Value']) if row['Parameter'] == 'GLUKOSA'
-                                          else "", axis=1)
-
-
-            # Convert the 'Value' column to string
-            result_df['Value'] = result_df['Value'].astype(str)
-            
-            result_df.index = result_df.index + 1
-            
-            st.table(result_df)
-    
-    else:
-        st.warning("Masukkan hanya gambar dipstick yang telah dicrop")
 
 def main():
     with st.sidebar:
